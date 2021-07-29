58289d690c7cfb2ae26402b1368d69c179bc5a61
rahn <ryanahn2000@gmail.com>
2021-07-29 09:14:08 -0700

    Initial Commit

---
+++ Programming Jokes/Programming_JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Programming_JokesApp.swift
+//  Programming Jokes
+//
+//  Created by Ryan Ahn on 7/29/21.
+//
+
+import SwiftUI
+
+@main
+struct Programming_JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
